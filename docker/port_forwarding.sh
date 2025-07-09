0e88adba233606698102ac411f80b2c22acca818
jar35 <jar35@st-andrews.ac.uk>
2025-07-09 16:28:54 +0100

    Set neo4j hostname via env
    Neo4J hostname is now settable via env variable.

--- docker/port_forwarding.sh
+++ docker/port_forwarding.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+HOST=${NEO4J_HOST:-umea-neo4j}
+
 # Forward ports to umea-neo4j container
-socat TCP-LISTEN:7474,fork TCP:umea-neo4j:7474 &
-socat TCP-LISTEN:7687,fork TCP:umea-neo4j:7687 &
+socat TCP-LISTEN:7474,fork TCP:$HOST:7474 &
+socat TCP-LISTEN:7687,fork TCP:$HOST:7687 &
